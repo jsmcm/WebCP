6a8c2119b636aa649300a7afd533493a70e8ee70
root <root@eu01.softsmart.co.za>
2019-03-07 11:20:27 +0100

    Checks for composer on index screen

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-include_once($_SERVER["DOCUMENT_ROOT"]."/vendor/autoload.php");
+
+if ( ! file_exists($_SERVER["DOCUMENT_ROOT"]."/vendor/autoload.php") ) {
+    print "<h1>Missing dependencies</h1><p>I'm going to try and install them. This might take several minutes. Please wait 30 minutes then try again</p><p>If you've already waited 30 minutes and still see this please contact support@webcp.io for support</p>";
+
+    if ( ! file_exists($_SERVER["DOCUMENT_ROOT"]."/nm/") ) {
+        mkdir($_SERVER["DOCUMENT_ROOT"]."/nm/", 0755);
+    }
+
+    touch($_SERVER["DOCUMENT_ROOT"]."/nm/composer_install");
+    exit();
+}
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/vendor/autoload.php");
 $oSettings = new Settings();
+
 ?>
 <!DOCTYPE html>
 
